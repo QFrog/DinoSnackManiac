a166d03a19040a6320c9a6d9a6a939eef63a2ad3
aukoferb4760 <60480098+aukoferb4760@users.noreply.github.com>
2020-02-10 13:43:48 -0600

    enemies move

--- DinoSnackManiac/Assets/Scripts/spawner.cs
+++ DinoSnackManiac/Assets/Scripts/spawner.cs
@@ -6,30 +6,33 @@ public class spawner : MonoBehaviour {
 
   //Enemy
   public GameObject cookieEnemyPrefab;
+  public GameObject spawnerPrefab;
   //Spawner Variables
   private int waveGrowth = 1;
   private int enemiesToSpawn;
   public float timeUntilSpawn = 0;
-  public float spawnCooldown = 1;
+  public float spawnCooldown = 30;
   private int rand = 1;
 
   //random number at start
   private void Start() {
-    int rand = Random.Range(1, 5);
+    int rand = Random.Range(1, 2);
   }
 
 
   // Update is called once per frame
   void Update() {
-    rand = Random.Range(1, 5);
-    timeUntilSpawn -= Time.deltaTime;
+    rand = Random.Range(1, 2);
 
+    timeUntilSpawn += Time.deltaTime;
+   
     //spawns cookies and establishes cooldown
-    if (timeUntilSpawn <= 5) {
+    if (timeUntilSpawn >= spawnCooldown) {
       spawnCookie();
-      timeUntilSpawn = spawnCooldown;
+      timeUntilSpawn = 0;
     }
-
+    
+    
   }
 
 
@@ -41,12 +44,14 @@ public class spawner : MonoBehaviour {
 
   private void spawnCookie() {
 
+    Vector3 spawnerPos = transform.position + new Vector3(rand, rand, 0);
+ 
     //Sets up spawn range and spawning of a single enemy
-    Vector3 pos = new Vector3(Random.Range(rand, rand), Random.Range(rand, rand), 0);
+    // Vector3 pos = new Vector3(Random.Range(rand, rand), Random.Range(rand, rand), 0);
 
     //Spawning a random amount of enemies
     for (enemiesToSpawn = (rand * waveGrowth); enemiesToSpawn >= 0; enemiesToSpawn--) {
-      GameObject spawner = Instantiate(cookieEnemyPrefab, pos, Quaternion.identity) as GameObject;
+      GameObject spawner = Instantiate(cookieEnemyPrefab, spawnerPos, Quaternion.identity) as GameObject;
     }
 
   }
