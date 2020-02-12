568bb7a6b1e79aae22b7bdb36412a093009c3a34
Huhnke <huhnkeq6627@uwstout.edu>
2020-02-12 12:25:25 -0600

    cookies now die

--- DinoSnackManiac/Assets/Scripts/CookieDeath.cs
+++ DinoSnackManiac/Assets/Scripts/CookieDeath.cs
@@ -10,16 +10,16 @@ public class CookieDeath : MonoBehaviour
         GameObject c1 = GameObject.Find("CookieType1(Clone)");
         GameObject c2 = GameObject.Find("CookieType2(Clone)");
         GameObject c3 = GameObject.Find("CookieType3(Clone)");
-        timer += 1.0F * Time.deltaTime;
         if (c1)
-            if (timer >= 4)
+            timer += 1.0F * Time.deltaTime;
+            if (timer >= 3)
             {
                 Destroy(c1.gameObject);
                 timer = 0;
             }
         if (c2)
         {
-
+            timer += 1.0F * Time.deltaTime;
             if (timer >= 4)
             {
                 Destroy(c2.gameObject);
@@ -28,12 +28,21 @@ public class CookieDeath : MonoBehaviour
         }
         if (c3)
         {
-
+            timer += 1.0F * Time.deltaTime;
             if (timer >= 4)
             {
                 Destroy(c2.gameObject);
                 timer = 0;
             }
+        }
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        UnityEngine.Debug.Log("Colliding on object: " + coll.gameObject.tag);
+
+        if (coll.gameObject.tag == "enemy")
+        {
+            Destroy(coll.gameObject);
         }
     }
 }
