18a9a9368c514250e0aa5b0c8c88a807309ff6b0
aukoferb4760 <60480098+aukoferb4760@users.noreply.github.com>
2020-02-23 17:27:56 -0600

    1321321

--- DinoSnackManiac/Assets/Scripts/MainMenu.cs
+++ DinoSnackManiac/Assets/Scripts/MainMenu.cs
@@ -4,27 +4,30 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour
-{
-    public GameObject startButton;
-    public GameObject startButton2;
-    public GameObject startText;
-    public GameObject tutText;
-    void Start()
-    {
-        tutText.SetActive(false);
-        startButton2.SetActive(false);
-    }
-    public void PlayGame()
-    {
-        SceneManager.LoadScene("MainScene");
-    }
-    public void StartButton()
-    {
-        startButton.SetActive(false);
-        startText.SetActive(false);
-        tutText.SetActive(true);
-        startButton2.SetActive(true);
+public class MainMenu : MonoBehaviour {
+  public GameObject startButton;
+  public GameObject startButton2;
+  public GameObject credits;
+  public GameObject startText;
+  public GameObject tutText;
+  void Start() {
+    tutText.SetActive(false);
+    startButton2.SetActive(false);
+  }
+  public void PlayGame() {
+    SceneManager.LoadScene("MainScene");
+  }
+
+  public void tutorial() {
+    SceneManager.LoadScene("Tutorial");
+  }
+
+  public void StartButton() {
+    startButton.SetActive(false);
+    startText.SetActive(false);
+    tutText.SetActive(true);
+    startButton2.SetActive(true);
+    credits.SetActive(true);
 
     }
 }
