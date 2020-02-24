d50a5b4850b1d206a0e82f7a556bc97c1ea4c008
Huhnke <huhnkeq6627@uwstout.edu>
2020-02-23 20:05:22 -0600

    Update PlayerController.cs

--- DinoSnackManiac/Assets/Scripts/PlayerController.cs
+++ DinoSnackManiac/Assets/Scripts/PlayerController.cs
@@ -16,11 +16,10 @@ public class PlayerController : MonoBehaviour
     public GameObject death2;
     public Image cross;
 
-  private Rigidbody2D rb2d;
-  private SpriteRenderer dino;
-  private Variables ammo;
-  private Animator anim;
-  private Animation animTest;
+    private Rigidbody2D rb2d;
+    private SpriteRenderer dino;
+    private Variables ammo;
+    public Animator anim;
 
     void Start()
     {
@@ -30,70 +29,86 @@ public class PlayerController : MonoBehaviour
         anim = GetComponent<Animator>();
         Time.timeScale = 1;
 
-    var cameraRect = new Rect(
-     bottomLeft.x,
-     bottomLeft.y,
-     topRight.x - bottomLeft.x,
-     topRight.y - bottomLeft.y);
+    }
+    void Update()
+    {
+        var delta = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        playerLocation = transform.position;
+        var bottomLeft = cam.ScreenToWorldPoint(Vector3.zero);
+        var topRight = cam.ScreenToWorldPoint(new Vector3(
+            cam.pixelWidth, cam.pixelHeight));
 
-    transform.position = new Vector3(Mathf.Clamp(transform.position.x, cameraRect.xMin, cameraRect.xMax),
-       Mathf.Clamp(transform.position.y, cameraRect.yMin, cameraRect.yMax), transform.position.z);
+        var cameraRect = new Rect(
+         bottomLeft.x,
+         bottomLeft.y,
+         topRight.x - bottomLeft.x,
+         topRight.y - bottomLeft.y);
 
-    if (delta.x >= 0 && !facingRight) { // mouse is on right side of player
-      dino.flipX = true; // activate look right some other way;
-      facingRight = true;
-    }
-    else if (delta.x < 0 && facingRight) { // mouse is on left side
-      dino.flipX = false; // activate look right some other way
-                          // activate looking left
-      facingRight = false;
-    }
-    playerMovement();
-  }
-  void FixedUpdate() {
-    anim.SetBool("Walking", true);
-    float moveHorizontonal = Input.GetAxis("Horizontal");
-    float moveVertical = Input.GetAxis("Vertical");
-    Vector2 movement = new Vector2(moveHorizontonal, moveVertical);
-    rb2d.velocity = movement * speed;
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, cameraRect.xMin, cameraRect.xMax),
+           Mathf.Clamp(transform.position.y, cameraRect.yMin, cameraRect.yMax), transform.position.z);
 
-  }
-  public void playerMovement() {
-    if (transform.position.y <= 2) {
-      // allowed to move
+        if (delta.x >= 0 && !facingRight)
+        { // mouse is on right side of player
+            dino.flipX = true; // activate look right some other way;
+            facingRight = true;
+        }
+        else if (delta.x < 0 && facingRight)
+        { // mouse is on left side
+            dino.flipX = false; // activate look right some other way
+            // activate looking left
+            facingRight = false;
+        }
+        playerMovement();
     }
-    else {
-      Vector3 newPosition = new Vector3(transform.position.x, 2, transform.position.z);
-      transform.position = newPosition;
-    }
-    if (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical")) {
-      anim.SetBool("Idle", false);
-      anim.SetBool("Walking", true);
-      
-      //print("Walking is true");
-    }
-    else {
-      anim.SetBool("Walking", false);
-      // print("Walking is false");
-    }
-    if (Input.GetButtonDown("Fire1")) {
-      anim.SetBool("Throwing", true);
-    }
-    else {
-      anim.SetBool("Throwing", false);
-    }
-  }
-  void OnTriggerEnter2D(Collider2D coll) {
+    void FixedUpdate()
+    {
+        float moveHorizontonal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontonal, moveVertical);
+        rb2d.velocity = movement * speed;
 
-    if (coll.gameObject.tag == "ammo") {
-      ammo.cookieAmmo1 += 10;
-      ammo.cookieText1.text = ammo.cookieAmmo1.ToString();
-      ammo.cookieAmmo2 += 10;
-      ammo.cookieText2.text = ammo.cookieAmmo2.ToString();
-      ammo.cookieAmmo3 += 10;
-      ammo.cookieText3.text = ammo.cookieAmmo3.ToString();
-      Destroy(coll.gameObject);
     }
+    public void playerMovement()
+    {
+        if (transform.position.y <= 2)
+        {
+            // allowed to move
+        }
+        else
+        {
+            Vector3 newPosition = new Vector3(transform.position.x, 2, transform.position.z);
+            transform.position = newPosition;
+        }
+        if (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical"))
+        {
+            anim.SetBool("Walking", true);
+        }
+        else
+        {
+            anim.SetBool("Walking", false);
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetBool("Throwing", true);
+        }
+        else
+        {
+            anim.SetBool("Throwing", false);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+
+        if (coll.gameObject.tag == "ammo")
+        {
+            ammo.cookieAmmo1 += 10;
+            ammo.cookieText1.text = ammo.cookieAmmo1.ToString();
+            ammo.cookieAmmo2 += 10;
+            ammo.cookieText2.text = ammo.cookieAmmo2.ToString();
+            ammo.cookieAmmo3 += 10;
+            ammo.cookieText3.text = ammo.cookieAmmo3.ToString();
+            Destroy(coll.gameObject);
+        }
     if (coll.gameObject.tag == "enemy" || coll.gameObject.tag == "enemy1" || coll.gameObject.tag == "enemy2") {
             Time.timeScale = 0;
             cross.enabled = false;
